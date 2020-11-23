45fa4deba4bd8aca64765151daedc580029fce05
jcquinlan <jcquinlan.dev@gmail.com>
2020-11-22 20:23:27 -0500

    use env for firebase config

--- services/index.js
+++ services/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfMnJxVcaJpEeob3W0M0hUlFkJaeXTkkI",
-    authDomain: "leerly.firebaseapp.com",
-    databaseURL: "https://leerly.firebaseio.com",
-    projectId: "leerly",
-    storageBucket: "leerly.appspot.com",
-    messagingSenderId: "873625576355",
-    appId: "1:873625576355:web:29e2a925b982aeff0d3016"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
