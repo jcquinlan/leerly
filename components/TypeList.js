ef5be7c6553fdce69a3a5e84e6d4f82c9a54b557
jcquinlan <jcquinlan.dev@gmail.com>
2020-12-10 22:37:10 -0500

    add colored pills for article types

--- components/TypeList.js
+++ components/TypeList.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Colors} from './styled';
 import {ArticleTypes} from '../services/articleService';
 
 const TypeList = ({types}) => {
     return (
         <TypeListWrapper>
-            <span>Genres: </span>
-            {types.map(type => {
-                return ArticleTypes[type];
-            }).join(', ')}
+            <TypeListContainer>
+            {
+                types.map(type => {
+                    return <TypePill color={Colors.ArticleTypes[type]}>{ArticleTypes[type]}</TypePill>
+                })
+            }
+            </TypeListContainer>
         </TypeListWrapper>
     )
 }
 
 export default TypeList;
 
+const TypeListContainer = styled.div`
+    display: flex;
+`
+const TypePill = styled.div`
+    display: inline-block;
+    background-color: ${props => props.color || '#666'};
+    margin-right: 5px;
+    padding: 5px 15px;
+    border-radius: 20px;
+`;
 const TypeListWrapper = styled.div`
     margin-bottom: 15px;
 
