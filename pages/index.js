c4122857eaa508627a08f512345a7698109fe15b
jcquinlan <jcquinlan.dev@gmail.com>
2020-12-20 20:39:36 -0500

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-// import Image from 'next/image';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
-import {Container, HeroWrapper, HeroContent, Divider, Title, Subtitle, Button, devices, Colors} from '../components/styled';
-import {MaskedText} from '../components/ArticlePreview';
+import ReactAudioPlayer from 'react-audio-player';
+import {
+  Container,
+  HeroWrapper,
+  HeroContent,
+  Divider,
+  Title,
+  Subtitle,
+  Button,
+  devices,
+  Colors,
+  AudioWrapper,
+  FakeAudioWidget
+} from '../components/styled';
+import {SimplifiedSelectedTextPopover} from '../components/SelectedTextPopover';
+import Data from '../landingPageData';
 
 function App() {
+  const exampleRef = useRef();
+  const [playAudio, setPlayAudio] = useState(false);
+  const [exampleOpen, setExampleOpen] = useState(false);
+
   return (
     <>
     <Container>
@@ -12,8 +29,11 @@ function App() {
         <MainHeroContent>
           <Title>leerly.</Title>
           <Subtitle>
-            Improve your Spanish by reading popular articles, all summarized and translated to B1/B2 Spanish by native speakers. 
-            <Faint> Qué guay.</Faint>
+            {/* Improve your Spanish by listening to and reading popular articles from major
+            new sites, all summarized and translated to B1/B2 Spanish by native speakers.  */}
+            Improve your Spanish by listening to and reading popular articles from major
+            new sites, all summarized and translated to B1/B2 Spanish by native speakers. 
+            <br /><Faint> Qué guay.</Faint>
           </Subtitle>
 
           <div style={{display: 'flex', justifyContent: 'center'}}>
@@ -22,62 +42,114 @@ function App() {
         </MainHeroContent>
       </HeroWrapper>
 
-      {/* <Divider /> */}
-
-      {/* <SectionHeader>How it works</SectionHeader>
-      <DescriptionText>We summarize and translate articles from your favorite sites, like the ones below.</DescriptionText>
-      <Step>
-        <StepNumber>1.)</StepNumber>
-        <div>
-          <StepTitle>We search for popular, relevant, widely-shared articles from around the web.</StepTitle>
-          <DescriptionText>
-            We try to use articles that are being shared a lot, and cover a wide range of topics, like tech,
-            business, art, news, science, culture, etc. We source news outlets, reddit, Hacker News, and more
-            to find what's being read around the web each day.
-          </DescriptionText>   
-        </div>
-      </Step>
-
-      <Step>
-        <StepNumber>2.)</StepNumber>
-        <div>
-          <StepTitle>We summarize and translate them to B1/B2 Spanish.</StepTitle>
-          <DescriptionText>
-            The crucial step. We aim to have everything accessible to intermediate learners. And no machine translations here,
-            just natural language from a native speaker. This way we can make sure you learn how native speakers actually talk,
-            and not how Google's fancy machine-learning models talk.
-          </DescriptionText>   
-        </div>
-      </Step>
-
-      <Step>
-        <StepNumber>3.)</StepNumber>
-        <div>
-          <StepTitle>We send them to you throughout the week. That's it.</StepTitle>
-          <DescriptionText>
-            You get to practice reading Spanish that's not <i>too</i> challenging while reading stuff you probably
-            already would have read in English, and without needing to find kid's books or young adult novels in Spanish.
-          </DescriptionText>   
-        </div>
-      </Step> */}
-
       <Divider />
 
-      <div>
-        <SectionHeader>Want an example?</SectionHeader>
+      <SectionHeader>Try it right here, right now</SectionHeader>
+      <DescriptionText>
+        Play the audio, and <Faint>highlight words to translate</Faint> to English.
+      </DescriptionText>
+
+      {!playAudio && (
+        <AudioWrapper>
+            <FakeAudioWidget onClick={() => setPlayAudio(true)}>
+                <span>Play audio</span> &#9658;
+            </FakeAudioWidget>
+        </AudioWrapper>
+      )}
+
+        {playAudio && (
+          <AudioWrapper>
+              <div>
+                  <ReactAudioPlayer
+                      src={Data.audioURL}
+                      controls
+                  />
+              </div>
+          </AudioWrapper>
+      )}
+
+      <SimplifiedSelectedTextPopover elementRef={exampleRef} articleBody={Data.article} />
+      <Example ref={exampleRef} open={exampleOpen}>
+          {Data.article}
+      </Example>
+
+      <ReadMoreWrapper>
+          <button onClick={() => setExampleOpen(!exampleOpen)}>{exampleOpen ? 'Hide article' : 'Read the rest of the article'}</button>
+      </ReadMoreWrapper>
+
+    </Container>
+
+      <TestimonialRow>
+        <Testimonial>
+          <Portrait>
+            <img src="/images/testimonials/marith.jpeg" />
+          </Portrait>
+          <TestimonialWrapper>
+            <Name>
+              Marith M.
+            </Name>
+
+            <Location>
+              Spanish hobbyist
+            </Location>
+
+            <Quote>
+              With leerly, I'm reading articles that are actually interesting, but at a skill level that doesn't feel too challenging for me.
+              Plus I feel like my vocabulary is expanding in ways that auto-translators wouldn't help with.
+            </Quote>
+          </TestimonialWrapper>
+        </Testimonial>
+
+        <Testimonial>
+          <Portrait>
+            <img src="/images/testimonials/elisabeth.jpeg" />
+          </Portrait>
+          <TestimonialWrapper>
+            <Name>
+              Elisabeth C.
+            </Name>
+
+            <Location>
+              B2 Spanish Student
+            </Location>
+
+            <Quote>
+              leerly is perfect because I’m reading articles that are interesting and relevant, but which also allow me to develop my vocabulary.
+            </Quote>
+          </TestimonialWrapper>
+        </Testimonial>
+{/* 
+        <Testimonial>
+          <Portrait>
+            <img src="http://www.fillmurray.com/200/200" />
+          </Portrait>
+          <TestimonialWrapper>
+            <Name>
+              Elisabeth Clumpkens
+            </Name>
+
+            <Location>
+              Richmond, VA
+            </Location>
+
+            <Quote>
+              leerly is perfect because I’m reading articles that are interesting and relevant, but also allow me to develop my Spanish vocabulary.
+            </Quote>
+          </TestimonialWrapper>
+        </Testimonial> */}
+      </TestimonialRow>
+
+    <Container>
+      <Divider />
+
+
+      <Question>
+        <SectionHeader>How much does it cost?</SectionHeader>
         <DescriptionText>
-          We take articles, like <a href="https://www.theguardian.com/australia-news/2020/oct/15/the-great-unravelling-i-never-thought-id-live-to-see-the-horror-of-planetary-collapse" target="_blank">this one</a> about the Australian wildfires from The Guardian, extract the core information and facts,
-          and present them in intermediate Spanish:
+          leerly costs <Free>$5/month</Free>. Sign up now to lock in this price forever, since prices will
+          be increasing as new features are rolled out.
         </DescriptionText>
-
-        <Example>
-          <MaskedText maxHeight="800px">
-              {exampleArticle}
-          </MaskedText>
-        </Example>
-      </div>
-
-      <Divider />
+      </Question>
 
       <Question>
       <SectionHeader>What do I get for $5/month?</SectionHeader>
@@ -95,7 +167,7 @@ function App() {
         </DescriptionText> */}
       </Question>
 
-      <Question>
+      {/* <Question>
       <SectionHeader>But why do I want this?</SectionHeader>
         <DescriptionText>
           For intermediate Spanish learners, it can be hard to find reading material which is understandable, but still interesting. We would know, we're trying
@@ -103,7 +175,7 @@ function App() {
           That means reading stuff we already wanted to read, but in Spanish which is perfectly suited to our level. Practicing Spanish is easier when
           you can do it while reading cool articles you like, or trashy celebrity drama (we probably won't translate trashy celebrity drama, don't worry).
         </DescriptionText>
-      </Question>
+      </Question> */}
 
       <Question>
         <SectionHeader>Do you have a trial?</SectionHeader>
@@ -118,24 +190,14 @@ function App() {
         </DescriptionText>
       </Question>
 
-      <Question>
-        <SectionHeader>How much does it cost?</SectionHeader>
-        <DescriptionText>
-          leerly costs <Free>$5/month</Free>. Sign up now to lock in this price forever, since prices will
-          be increasing as new features are rolled out.
-        </DescriptionText>
-      </Question>
-
-      <Question>
+      {/* <Question>
         <SectionHeader>Can't I just use Google Translate or something?</SectionHeader>
         <DescriptionText>
           Sure! But we've found that it's best to have a native Spanish speaker rewrite the articles, to ensure that the Spanish
           is targeted specifically for intermediate learners. If you're an advanced speaker, you might not need this. Plus,
           you wouldn't get all the other neat features we're building.
         </DescriptionText>
-      </Question>
-
-
+      </Question> */}
 
       <HeroWrapper>
         <HeroContent>
@@ -160,15 +222,63 @@ function App() {
 
 export default App;
 
-const exampleArticle = `Estos días pienso mucho en las últimas semanas de la vida de mi padre. Recuerdo su lucha contra la enfermedad y sus ganas de vivir. También recuerdo el día en que supe que íbamos a perderlo. Fue como un punto de no retorno: un día vi la muerte en su cara, y me di cuenta de que ya no podíamos hacer nada más por él.
+const TestimonialRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 15px;
+  margin-top: 30px;
+  align-items: center;
 
-Soy científica, e investigo sobre el calentamiento global. Mi trabajo consiste en elaborar informes para el Grupo Intergubernamental de Expertos sobre el Cambio Climático (IPCC), un organismo de las Naciones Unidas (ONU). Gracias a mis conocimientos, comprendo muy bien el estado de nuestro planeta, y ahora mismo estoy aterrorizada. Reconozco los incendios del verano pasado en Australia: son un punto de no retorno, como el que vi aquel día en la cara de mi padre.
+  @media ${devices.laptop} {
+    flex-direction: row;
+    margin-bottom: 90px;
+    align-items: flex-start;
+  }
+`;
+const Testimonial = styled.div`
+  margin-bottom: 30px;
+  padding: 15px;
+  display: flex;
+  border: 1px solid #eee;
+  border-radius: 8px;
+  max-width: 400px;
+  margin-bottom: 30px;
 
-Durante el Verano Negro Australiano se quemó el 20% del matorral del país. Más de tres mil millones de animales murieron quemados o perdieron su hábitat para siempre. El Gran Arrecife de Coral enfermó gravemente. La tierra necesitará varias generaciones humanas para curarse de todo ese daño. Las consecuencias del calentamiento global ya no son amenazas lejanas e improbables. Están aquí. Forman parte de nuestra experiencia vivida.
+  &:last-child {
+    margin-right: 0;
+  }
 
-Los científicos intentamos movilizar a la sociedad con hechos y datos, pero en estos momentos siento que nuestros esfuerzos son inútiles. Los gobernantes no nos escuchan; prefieren proteger a las industrias de combustibles fósiles y a las empresas contaminantes. La ciudadanía sí comprende el problema y quiere ayudar, pero las soluciones prácticas o individuales como el reciclaje o las energías renovables no son suficientes. Creo que los datos científicos tampoco bastan: es necesaria una revolución emocional.
-Más que comprenderla, necesitamos sentir la enorme pérdida de nuestro planeta Tierra. Debemos vivir el luto colectivo de un mundo que amamos y que nunca va a volver a ser igual. Tenemos que llorar por nuestro futuro y el de nuestros hijos. Cuando abrimos la puerta a todos esos sentimientos intensos y complejos, nos llenan de energía y de ganas de actuar. Aceptar nuestra tristeza profunda, sin evitarla ni racionalizarla, es la clave para frenar el desastre.
-No sé cómo vivir al borde del colapso planetario. No sé cómo continuar creyendo en mi trabajo cuando me siento tan cansada y apenada. A veces, intento consolarme pensando que la muerte forma parte de la vida: que igual que no pudimos salvar a mi padre, no podemos salvar el planeta. Pero en esos momentos de intenso dolor también siento esperanza. Quizás, si conseguimos que nuestra pena compartida nos despierte, encontraremos la fuerza necesaria para actuar urgentemente y salvar la Tierra y a nosotros mismos.`;
+  @media ${devices.laptop} {
+    margin-bottom: 0;
+    margin-right: 30px;
+
+`;
+const Portrait = styled.div`
+  width: 50px;
+  height: 50px;
+  margin-right: 15px;
+
+  img {
+    width: 50px;
+    border-radius: 25px;
+  }
+`;
+const TestimonialWrapper = styled.div``;
+const Name = styled.p`
+  margin: 0;
+  font-weight: bold;
+`;
+const Location = styled.p`
+  margin: 0;
+  font-size: 12px;
+  color: #666;
+  margin-bottom: 5px;
+`;
+const Quote = styled.p`
+  margin: 0;
+  line-height: 26px;
+`;
 
 const FeaturesList = styled.ul`
   font-size: 18px;
@@ -183,7 +293,7 @@ const SectionHeader = styled(Subtitle)`
 `;
 const MainHeroContent = styled(HeroContent)`
   @media ${devices.laptop} {
-    margin: 90px 0;
+    margin: 90px 0 30px 0;
   }
 `;
 const Faint = styled.span`
@@ -191,40 +301,28 @@ const Faint = styled.span`
   font-family: 'Poppins', sans-serif;
 `;
 
-const Step = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 30px;
-
-  @media ${devices.laptop} {
-    flex-direction: row;
-  }
-`;
-
-const StepNumber = styled.div`
-  display: flex;
-  align-items: center;
-  padding-right: 20px;
-  font-size: 24px;
-  margin-bottom: 15px;
-
-  @media ${devices.laptop} {
-    font-size: 48px;
-    justify-content: center;
-    margin-bottom: 0px;
-  }
-`;
-
-const StepTitle = styled.h5`
-  font-size: 18px;
-  margin: 0;
-`;
-
 const Example = styled.div`
   margin: 10px 0 30px 0;
   white-space: pre-wrap;
   border-radius: 5px;
   padding: 30px;
+  line-height: 30px;
+  font-size: 16px;
+  height: ${props => props.open ? '100%' : '308px'};
+  overflow-y: hidden;
+`;
+
+const ReadMoreWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button {
+    background-color: transparent;
+    border: none;
+    color: ${Colors.Primary};
+    font-size: 18px;
+    cursor: pointer;
+  }
 `;
 
 const DescriptionText = styled.p`
