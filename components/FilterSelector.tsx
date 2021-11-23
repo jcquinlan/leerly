b5859d244d8ae7ab12480a144c1956a278efc4d8
James Quinlan <jcquinlan.dev@gmail.com>
2021-11-23 11:39:13 +0100

    pill styles

--- components/FilterSelector.tsx
+++ components/FilterSelector.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import Select from 'react-select';
-import {ArticleTypeList} from '../services/articleService';
-import {ArticleType} from 'types';
+import styled from 'styled-components';
+import Select, { components } from 'react-select';
+import { ArticleTypeList } from '../services/articleService';
+import { ArticleType } from 'types';
+import { Colors } from './styled';
+
+const customStyles = {
+    multiValue: (styles, { data }) => {
+        return {
+            ...styles,
+            backgroundColor: Colors.ArticleTypes.background[data.value],
+            borderRadius: '20px',
+            paddingLeft: 5
+        };
+    },
+    multiValueLabel: (styles, { data }) => ({
+        ...styles,
+        color: Colors.ArticleTypes.copy[data.value]
+    }),
+    multiValueRemove: (styles, { data }) => ({
+        ...styles,
+        borderRadius: '0 20px 20px 0',
+        cursor: 'pointer'
+    })
+}
 
 interface FilterSelectorProps {
     onChange: (selectedArticleTypes: ArticleType[]) => void;
 }
-const FilterSelector = ({onChange}: FilterSelectorProps) => {
+const FilterSelector = ({ onChange }: FilterSelectorProps) => {
     const options = ArticleTypeList.map((entry) => {
         return {
             value: entry.type,
@@ -21,9 +43,17 @@ const FilterSelector = ({onChange}: FilterSelectorProps) => {
             isClearable
             onChange={onChange}
             placeholder="Select up to 10 filters..."
-            style={{fontWeight: 300}}
+            style={{ fontWeight: 300 }}
+            styles={customStyles}
         />
     )
 }
 
 export default FilterSelector;
+
+const GenrePill = styled.div<{ backgroundColor: string }>`
+    padding: 5px 7px;
+    border-radius: 20px;
+    color: ${props => props.color};
+    backgroundColor: ${props => props.backgroundColor};
+`;
