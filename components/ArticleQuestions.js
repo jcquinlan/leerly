3aefd6f1809ed14071d8b7cd433170baeeb6235c
James Quinlan <jamesquinlan@jamess-mbp-2.mynetworksettings.com>
2022-05-11 20:36:31 -0400

    remove comment

--- components/ArticleQuestions.js
+++ components/ArticleQuestions.js
@@ -73,7 +73,6 @@ export default function ArticleQuestions({articleId, questions}) {
     }
   };
 
-  // RENDER //////////////////////////////////////////////////////////////////
   return (
     <QuestionsContainer>
       <QuestionsWrapper>
